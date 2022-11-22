3eee55e53d685567d8ac3f44fbd602a6155777cc
chhun rotanakkosal <rotanakkosal03@gmail.com>
2022-11-22 16:23:33 +0700

    Push code

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function imgOver(){
+   document.getElementById("top-new").src="./../img/kansan-top-005.jpg";
+}
+
+function imgOut(){
+   document.getElementById("top-new").src="./../img/top-new-r-001.jpg";
+}
+
+
+function imgLeftOver(){
+   document.getElementById("top-left-news").src="./../img/home-left-001.jpg";
+}
+function imgLeftOut(){
+   document.getElementById("top-left-news").src="./../img/home-left-002.jpg";
+}
+
+
+function titleSpacing() {
+   document.getElementById("title-menu").style.letterSpacing = "3px";
+   document.getElementById("title-menu").style.fontStyle = "italic";
+}
+
+function titleOutSpecing(){
+   document.getElementById("title-menu").style.letterSpacing = "0px";
+   document.getElementById("title-menu").style.fontStyle = "normal";
+}
